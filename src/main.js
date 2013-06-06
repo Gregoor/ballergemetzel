55489faa08336f24797b427235a2266118df9b3c
Gregor Weber <gregor.weber@antevorte.org>
2013-06-07 01:09:58 +0200

    Game loop

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 require([], function() {
-	document.getElementsByTagName("body")[0].innerHTML = "<h1>IT WORKS!</h1>";
+	var update = function(delta) {
+			console.log(delta);
+		},
+		draw = function(delta) {
+
+		},
+		mainloop = function(delta) {
+			update(delta);
+			draw(delta);
+		};
+
+	var animFrame = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		null;
+
+	if (animFrame !== null) {
+		var canvas, prevTime = 0;
+
+		var recursiveAnim = function(time) {
+			var delta = time - prevTime;
+			prevTime = time;
+
+			mainloop(delta);
+			animFrame(recursiveAnim, canvas);
+		};
+
+		animFrame(recursiveAnim, canvas);
+	} else {
+		var ONE_FRAME_TIME = 1000.0 / 60.0;
+		setInterval(function() {
+			mainLoop(ONE_FRAME_TIME);
+		}, ONE_FRAME_TIME);
+	}
 });
