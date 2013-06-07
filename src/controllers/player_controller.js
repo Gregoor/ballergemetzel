1c5ad6f7a48eadee5238b755e8310362027d0043
Gregor Weber <gregor.weber@antevorte.org>
2013-06-07 05:04:06 +0200

    Switched to double quotes

--- src/controllers/player_controller.js
+++ src/controllers/player_controller.js
@@ -1,27 +1,26 @@
 define(function() { return function PlayerController(player) {
 		this.player = player;
-		console.log(this.player);
 		var that = this;
 
 		this.keyEvent = function(key) {
 			key = key || window.event;
-			keyCode = key.keyCode;
+			var keyCode = key.keyCode;
 			switch(keyCode) {
 				case 37: // left
-					that.player.move('l');
+					that.player.move("l");
 					break;
 				case 39: // right
-					that.player.move('r');
+					that.player.move("r");
 					break;
 				case 38: // up
-					that.player.move('u');
+					that.player.move("u");
 					break;
 				case 40: // down
-					that.player.move('d');
+					that.player.move("d");
 					break;
 				default:
 					//console.log(that.player);
-					//alert('caught unexpected keycode '+keyCode)
+					//alert("caught unexpected keycode "+keyCode)
 			}
 		}
 
--- src/models/player.js
+++ src/models/player.js
@@ -1,42 +1,48 @@
-define(["kinetic"], function(Kinetic) { return function Player(x,y) {
+define(["kinetic"], function(Kinetic) {
+	return function Player(x, y) {
 		this.x = x;
 		this.y = y;
-		this.direction = 'l';
+		this.direction = "l";
 		this.image = new Image();
-		this.image.src = 'http://www.html5canvastutorials.com/demos/assets/blob-sprite.png';
+		this.image.src = "http://www.html5canvastutorials.com/demos/assets/blob-sprite.png";
 		this.animations = {
-        	idle: [{
-          		x: 2,
-          		y: 2,
-          		width: 70,
-          		height: 119
-        	}, {
-          		x: 71,
-          		y: 2,
-          		width: 74,
-          		height: 119
-        	}, {
-          		x: 146,
-         		y: 2,
-          		width: 81,
-          		height: 119
-        	}, {
-          		x: 226,
-          		y: 2,
-          		width: 76,
-          		height: 119
-        	}]
-        };
+			idle: [
+				{
+					x: 2,
+					y: 2,
+					width: 70,
+					height: 119
+				},
+				{
+					x: 71,
+					y: 2,
+					width: 74,
+					height: 119
+				},
+				{
+					x: 146,
+					y: 2,
+					width: 81,
+					height: 119
+				},
+				{
+					x: 226,
+					y: 2,
+					width: 76,
+					height: 119
+				}
+			]
+		};
 
 		this.view = new Kinetic.Sprite({
-        	x: this.x,
-         	y: this.y,
-          	image: this.image,
-          	animation: 'idle',
-          	animations: this.animations,
-          	frameRate: 15,
-          	index: 0
-        });
+			x: this.x,
+			y: this.y,
+			image: this.image,
+			animation: "idle",
+			animations: this.animations,
+			frameRate: 15,
+			index: 0
+		});
 
 		this.renderTo = function(layer) {
 			layer.add(this.view);
@@ -47,31 +53,33 @@ define(["kinetic"], function(Kinetic) { return function Player(x,y) {
 		this.move = function(direction) {
 			var xOffset = 20;
 			var yOffset = 15;
-			switch(direction) {
-				case 'l':
+			switch (direction) {
+				case "l":
 					this.x -= xOffset;
 					break;
-				case 'r':
+				case "r":
 					this.x += xOffset;
 					break;
-				case 'u':
+				case "u":
 					this.y -= yOffset;
 					break;
-				case 'd':
+				case "d":
 					this.y += yOffset;
 					break;
 			}
 
-			this.getStageAttr = function(attr) { return this.view.getLayer().getStage().getAttr(attr); }
+			this.getStageAttr = function(attr) {
+				return this.view.getLayer().getStage().getAttr(attr);
+			}
 
-			if(this.x <= 0)
-				this.x += this.getStageAttr('width');
-			else if(this.x >= this.getStageAttr('width'))
-				this.x -= this.getStageAttr('width');
-			if(this.y <= 0)
-				this.y += this.getStageAttr('height');
-			else if(this.y >= this.getStageAttr('height'))
-				this.y -= this.getStageAttr('height');
+			if (this.x <= 0)
+				this.x += this.getStageAttr("width");
+			else if (this.x >= this.getStageAttr("width"))
+				this.x -= this.getStageAttr("width");
+			if (this.y <= 0)
+				this.y += this.getStageAttr("height");
+			else if (this.y >= this.getStageAttr("height"))
+				this.y -= this.getStageAttr("height");
 			this.view.setX(this.x);
 			this.view.setY(this.y);
 			//alert("x is now "+this.x);
